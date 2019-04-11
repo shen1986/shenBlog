9eb6eeaa9273a2d85f0d897fd5d03c15870c415a
shenxf1986 <185693240@qq.com>
2019-04-11 21:34:16 +0800

    update

--- dynamic-koa/blog/controller/timeline.js
+++ dynamic-koa/blog/controller/timeline.js
@@ -3,10 +3,15 @@
  * @Author: shenxf
  * @Date: 2019-03-28 16:22:17
  */
+const timelineModel = require('../models/timeline');
 
 // 获得归档页
 exports.getTimeline = async function (ctx) {
+
+    var timeline = await timelineModel.getTimeline();
+
     await ctx.render("timeline.art", {
+        ...timeline,
         ...ctx.res.$initValue,
         common: {
             hasBanner: false
---
+++ dynamic-koa/blog/models/timeline.js
@@ -0,0 +1,64 @@
+/*
+ * @Description: 慢生活模型
+ * @Author: shenxf
+ * @Date: 2019-04-10 22:08:51
+ */
+var db = require("./db");
+
+var getTimeline = async function (current = 1, count = 30, category = 0) {
+
+	let sqls = [
+		"select id, theme from category where status = 1"
+	];
+		
+	let sql = "";
+	let field = "article.id, title, created_at";
+
+	if(category == 0) {
+		sql = `select ${field} from article where article.status = 1
+			order by created_at desc limit ${(+current - 1) * +count}, ${+count}`;
+		sqls.push(sql);
+
+		sql = 'select count(*) as total from article where status = 1';
+		sqls.push(sql);
+	} else{
+		sql = `select ${field} from article where category = ${+category} and article.status = 1 
+			order by created_at desc limit ${(+current - 1) * +count}, ${+count}`;
+		sqls.push(sql);
+
+		sql = `select count(*) as total from article where status = 1 and category = ${+category}`;
+
+		sqls.push(sql);
+	}
+
+	let ps = [];
+	for (sql of sqls) {
+		ps.push(new Promise(function(resolve, reject) {
+			db.query(sql, function(err, rows) {
+				if(err) {
+					reject(err);
+				}
+				resolve(rows);
+			})
+		}))
+	}
+
+    try {
+        let p = await Promise.all(ps);
+    
+        result(p);
+    } catch (error) {
+        return {"status": 0, "message": '系统异常'};
+    }
+}
+
+var result = function(out) {
+    return {
+        status: 1,  
+        categories: out[0],
+        items: out[1],
+        total: out[2][0]['total']
+    };
+}
+
+module.exports.getTimeline = getTimeline;
