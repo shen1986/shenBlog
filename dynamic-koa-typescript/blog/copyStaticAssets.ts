15afa923e9afb5c9601042342fe7a02fde30255e
shenxf1986 <185693240@qq.com>
2019-04-17 21:42:35 +0800

    update

--- dynamic-koa-typescript/blog/copyStaticAssets.ts
+++ dynamic-koa-typescript/blog/copyStaticAssets.ts
@@ -1,9 +1,12 @@
+/**
+ * @Description: 共通复制
+ * @Author: shenxf
+ * @Date: 2019-04-17 20:38:11
+ */
 import * as shell from 'shelljs';
 
 shell.cp('-R', 'src/public/js', 'dist/public/');
-shell.cp('-R', 'src/public/fonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
 shell.cp('-R', 'src/public/icoFonts', 'dist/public/');
 shell.cp('-R', 'src/public/lib', 'dist/public/'); // 第三方js
 shell.cp('-R', 'src/public/favicon.ico', 'dist/public/');
-
--- dynamic-koa-typescript/blog/src/models/db.ts
+++ dynamic-koa-typescript/blog/src/models/db.ts
@@ -9,8 +9,8 @@ import mysql from 'mysql';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'blog',
+    password: '580114',
+    database: 'blog2',
     dateStrings: true
 });
 
