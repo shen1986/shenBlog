06a55cad7670ad3ceb9c4387715280ff92b223d3
shenxf1986 <185693240@qq.com>
2019-04-21 09:05:33 +0800

    update

--- vueProject/admin/src/App.vue
+++ vueProject/admin/src/App.vue
@@ -4,33 +4,33 @@
  * @Date: 2019-04-20 21:05:11
  -->
 <template>
-    <div>
-        Name: <input v-model="name" type="text">
-        <h1>Hello Component</h1>
-        <helloComponent :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Decorator Component</h1>
-        <hello-decorator-component :name="name" :initialEnthusiasm="5" />
-    </div>
+    <Layout>
+        <Header>header</Header>
+        <Layout>
+            <Sider>left sidebar</Sider>
+            <Content>main content</Content>
+        </Layout>
+        <Footer>footer</Footer>
+    </Layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import helloComponent from './components/Hello.vue';
+import helloComponent from "./components/Hello.vue";
 import HelloDecoratorComponent from "./components/HelloDecorator.vue";
 
 export default Vue.extend({
-    data() {
-        return {
-            name: "World"
-        };
-    },
-    components: {
-        helloComponent,
-        HelloDecoratorComponent
-    },
+  data() {
+    return {
+      name: "World"
+    };
+  },
+  components: {
+    helloComponent,
+    HelloDecoratorComponent
+  }
 });
 </script>
 
 <style>
-
 </style>
--- vueProject/admin/src/index.ts
+++ vueProject/admin/src/index.ts
@@ -4,16 +4,15 @@
  * @Date: 2018-03-24 23:04:44
  */
 import Vue from "vue";
-import HelloComponent from "./components/Hello.vue";
-import HelloDecoratorComponent from "./components/HelloDecorator.vue";
 import app from "./App.vue";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.config.productionTip = false
+
+// 引入蚂蚁式样
+Vue.use(Antd)
 
 let v = new Vue({
     el: "#app",
     render: c => c(app),
-    data: { name: "World" },
-    // components: {
-    //     HelloComponent,
-    //     HelloDecoratorComponent
-    // }
 });
