3310414c161b093111b35a4a11c1f1ded75f061b
shenxf1986 <185693240@qq.com>
2019-04-29 10:10:59 +0800

    axios基础配置追加

--- vueProject/admin/src/index.ts
+++ vueProject/admin/src/index.ts
@@ -20,6 +20,25 @@ import router from './router/router';
 import Axios from 'axios';
 // 设置Axios基础配置
 Axios.defaults.baseURL = 'http://127.0.0.1';
+Axios.defaults.timeout = 20000;
+Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+
+// 请求拦截
+// Axios.interceptors.request.use(
+//     config => {
+//         /*
+//          * 每次发送请求之前判断vuex中是否存在token
+//          * 如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录状况
+//          * 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
+//          */
+//         const token = store.state.token;
+//         token && (config.headers.Authorization = token);
+//         return config;
+//     },
+//     error => {
+//         return Promise.error(error);
+//     }
+// );
 
 // 将Axios挂载到Vue的原型链上
 Vue.prototype.$axios = Axios;
