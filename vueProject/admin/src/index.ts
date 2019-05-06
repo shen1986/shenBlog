199bcf5a994a2875cedc5828c7f1faf232ac4ea6
shenxf1986 <185693240@qq.com>
2019-05-06 12:36:48 +0800

    改变BaseURL 追加请求拦截

--- vueProject/admin/src/index.ts
+++ vueProject/admin/src/index.ts
@@ -19,26 +19,27 @@ import router from './router/router';
 // 导入Axios
 import Axios from 'axios';
 // 设置Axios基础配置
-Axios.defaults.baseURL = 'http://127.0.0.1';
+Axios.defaults.baseURL = 'http://127.0.0.1:8080';
 Axios.defaults.timeout = 20000; // 20秒超时
 Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 
 // 请求拦截
-// Axios.interceptors.request.use(
-//     config => {
-//         /*
-//          * 每次发送请求之前判断vuex中是否存在token
-//          * 如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录状况
-//          * 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
-//          */
-//         const token = store.state.token;
-//         token && (config.headers.Authorization = token);
-//         return config;
-//     },
-//     error => {
-//         return Promise.error(error);
-//     }
-// );
+Axios.interceptors.request.use(
+    config => {
+        /*
+         * 每次发送请求之前判断vuex中是否存在token
+         * 如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录状况
+         * 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
+         */
+        const token = store.state.token;
+        token && (config.headers.Authorization = token);
+        return config;
+    },
+    error => {
+        // return Promise.error(error);
+        console.log(error.message);
+    }
+);
 
 // 将Axios挂载到Vue的原型链上
 Vue.prototype.$axios = Axios;
@@ -55,7 +56,7 @@ Vue.use(Vuex);
 
 var store = new Vuex.Store({
     state: { // 全局变量
-        count: 1
+        token: '' // 用户验证
     },
     mutations: { // 全局方法
 
